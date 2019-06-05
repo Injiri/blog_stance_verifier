47521586e1d57a4dd8d3a80b5e3f28081a7350ea
Injiri <simoninjiri@gmail.com>
2019-06-05 16:41:21 +0300

    web app set pending server intergration

--- web_app/app.py
+++ web_app/app.py
@@ -1,10 +1,18 @@
 import flask
-
+import pandas as pd
 app = flask.Flask(__name__, template_folder='templates')
 
-@app.route('/')
-def main():
-    return (flask.render_template('main.html'))
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    if flask.request.method == 'GET':
+        return (flask.render_template('main.html'))
+
+    if flask.request.method == 'POST':
+        blog_url = flask.request.form['url']
+
+        input_variable = pd.DataFrame([blog_url])
+
+        prediction = model.predict(input_variable)[0]
+
+        return flask.render_template('main.html', input_blog_url = { 'input_blog_url': blog_url})
